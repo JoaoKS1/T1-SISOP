7e028643c9ef57605029ca045b5ec6f6460727a3
Rodrigo Goncalves <rodrigogoncalves2408@gmail.com>
2026-03-25 20:25:10 -0300

    Subindo arquivo de Sistema e Gerenciado de Memoria a serem trabalhados

---
+++ GerenciadorMemoria.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public interface GerenciadorMemoria {
+
+    // Retorna true e o vetor que em que página o frame será alocado
+    ArrayList<Integer> aloca(int nroPalavras);
+
+    // Libera frames alocados
+    void desaloca(ArrayList<Integer> paginasLivres);
+
+}
